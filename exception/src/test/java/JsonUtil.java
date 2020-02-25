1595ebf62b81e188b35eb144d756e7fa790e7a45
duyuqian <15738381799@163.com>
2020-02-25 22:18:12 +0800

    [duyuqian] feat: complete the convertToJson method to JsonUtil class

--- exception/src/test/java/JsonUtil.java
+++ exception/src/test/java/JsonUtil.java
@@ -1,15 +1,21 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class JsonUtil {
 
-  private JsonUtil() {
-  }
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+    private JsonUtil() {
+    }
 
-  public static String convertToJson(Object object) {
-    //TODO: change the code to pass the test
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    throw new NotImplementedException();
-  }
+    public static String convertToJson(Object object) {
+        //TODO: change the code to pass the test
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            System.out.println(e);
+        }
+        throw new NotImplementedException();
+    }
 }
